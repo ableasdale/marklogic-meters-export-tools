3226b6620ba67ffd14c8125218d77b49bb7db518
Alex Bleasdale <ableasdale@marklogic.com>
2018-07-22 15:33:11 +0100

    Created loop for json data returned by service

--- src/main/xquery/multicharts.js
+++ src/main/xquery/multicharts.js
@@ -4,28 +4,14 @@ params = request.substring(request.indexOf("?"));
 
 async function chart() {
     const data = await d3.json("/multicharts.sjs"+params);
-    //console.dir(data.layout);
 
-    // TODO - ultimately do these in a loop rather than hard code them
-    ReactDOM.render(
-        React.createElement(Plot, data[0]),
-        document.getElementById('root0')
-      );
-
-    ReactDOM.render(
-        React.createElement(Plot, data[1]),
-        document.getElementById('root1')
-    );
-
-    ReactDOM.render(
-        React.createElement(Plot, data[2]),
-        document.getElementById('root2')
-    );
-
-    ReactDOM.render(
-        React.createElement(Plot, data[3]),
-        document.getElementById('root3')
-    );
+    // console.log(Object.keys(data).length);
+    for(var i = 0; i < Object.keys(data).length; i++) {
+        ReactDOM.render(
+            React.createElement(Plot, data[i]),
+            document.getElementById('root'+i)
+        );
+    }
 }
 
 chart();
